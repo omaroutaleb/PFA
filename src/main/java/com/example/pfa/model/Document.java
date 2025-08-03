777dd311a5a3f8761dbd473935d668510858a538
OMAR OUTALEB <ooutaleb5@gmail.com>
2025-08-03 18:09:09 +0100

    fourth

--- src/main/java/com/example/pfa/model/Document.java
+++ src/main/java/com/example/pfa/model/Document.java
@@ -1,4 +1,18 @@
 package com.example.pfa.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileNam;
+    private String docType;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // Getters and setters
 }

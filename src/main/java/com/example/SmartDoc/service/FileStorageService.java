43c197cbb77ec2bf20b854909706038ba25f0c8e
OMAR OUTALEB <ooutaleb5@gmail.com>
2025-08-10 17:53:14 +0100

    ten

--- src/main/java/com/example/SmartDoc/service/FileStorageService.java
+++ src/main/java/com/example/SmartDoc/service/FileStorageService.java
@@ -1,10 +1,26 @@
 package com.example.SmartDoc.service;
 
+import com.example.SmartDoc.model.Document;
+import org.apache.tomcat.util.http.fileupload.impl.SizeException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 
 @Service
 public class FileStorageService {
+
+    public File save(MultipartFile file ) {
+
+        try {
+            if(file.isEmpty() || file.getSize() > 10 ) {
+                
+            }
+        }catch (Exception e ){
+            System.out.println( e.getMessage() );;
+        }
+
+    }
 
 }

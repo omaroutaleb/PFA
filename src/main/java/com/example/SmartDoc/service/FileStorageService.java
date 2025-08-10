8654d43ba5a7a99a8e9cb9d0da67e56ef0d3ec69
ayoubaitbarka <ayoubaitbarka2003@gmail.com>
2025-08-10 18:06:23 +0100

    nothing

--- src/main/java/com/example/SmartDoc/service/FileStorageService.java
+++ src/main/java/com/example/SmartDoc/service/FileStorageService.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
 
+import com.example.SmartDoc.adapter.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;

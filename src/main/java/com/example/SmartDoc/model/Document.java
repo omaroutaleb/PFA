3423fd29f90815654f928778a3f19c6c673c6956
ayoubaitbarka <ayoubaitbarka2003@gmail.com>
2025-08-10 17:56:18 +0100

    nothing

--- src/main/java/com/example/SmartDoc/model/Document.java
+++ src/main/java/com/example/SmartDoc/model/Document.java
@@ -28,4 +28,52 @@ public class Document {
     private LocalDateTime createdAt;
     private String status;
     private String extractedDataJson;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getDocType() {
+        return docType;
+    }
+
+    public void setDocType(String docType) {
+        this.docType = docType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getExtractedDataJson() {
+        return extractedDataJson;
+    }
+
+    public void setExtractedDataJson(String extractedDataJson) {
+        this.extractedDataJson = extractedDataJson;
+    }
 }
--- src/main/java/com/example/SmartDoc/service/FileStorageService.java
+++ src/main/java/com/example/SmartDoc/service/FileStorageService.java
@@ -1,10 +1,96 @@
 package com.example.SmartDoc.service;
 
+//
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@Service
-public class FileStorageService {
+//
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+
+@Service
+public class FileStorageService{
+
+
+    private final Path rootLocation;
+
+    @Autowired
+    public FileStorageService(StorageProperties properties) {
+
+        if(properties.getLocation().trim().length() == 0){
+            throw new StorageException("File upload location can not be Empty.");
+        }
+
+        this.rootLocation = Paths.get(properties.getLocation());
+    }
+
+    public void init() {
+
+    }
+
+    public void store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file.");
+            }
+            Path destinationFile = this.rootLocation.resolve(
+                            Paths.get(file.getOriginalFilename()))
+                    .normalize().toAbsolutePath();
+            if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file outside current directory.");
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile,
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to store file.", e);
+        }
+    }
+
+    public Stream<Path> loadAll() {
+        return Stream.empty();
+    }
+
+
+    public Path load(String filename) {
+        return null;
+    }
+
+
+    public Resource loadAsResource(String filename) {
+        return null;
+    }
+
+
+    public void deleteAll() {
+
+    }
 }

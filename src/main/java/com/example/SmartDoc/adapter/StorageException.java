2a2938c09870b176ca9b2478ea39b59c23738aca
ayoubaitbarka <ayoubaitbarka2003@gmail.com>
2025-08-10 18:23:05 +0100

    nothing

--- src/main/java/com/example/SmartDoc/adapter/StorageException.java
+++ src/main/java/com/example/SmartDoc/adapter/StorageException.java
@@ -3,4 +3,7 @@ package com.example.SmartDoc.adapter;
 public class StorageException extends Throwable {
     public StorageException(String s) {
     }
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/com/example/SmartDoc/service/FileStorageService.java
+++ src/main/java/com/example/SmartDoc/service/FileStorageService.java
@@ -4,6 +4,7 @@ package com.example.SmartDoc.service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,7 +15,9 @@ import com.example.SmartDoc.adapter.StorageException;
 import com.example.SmartDoc.adapter.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -28,7 +31,7 @@ public class FileStorageService{
     private final Path rootLocation;
 
     @Autowired
-    public FileStorageService(StorageProperties properties) {
+    public FileStorageService(StorageProperties properties) throws StorageException {
 
         if(properties.getLocation().trim().length() == 0){
             throw new StorageException("File upload location can not be Empty.");
@@ -37,11 +40,16 @@ public class FileStorageService{
         this.rootLocation = Paths.get(properties.getLocation());
     }
 
-    public void init() {
-
+    public void init() throws StorageException {
+        try {
+            Files.createDirectories(rootLocation);
+        }
+        catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
     }
 
-    public void store(MultipartFile file) {
+    public void store(MultipartFile file) throws StorageException {
         try {
             if (file.isEmpty()) {
                 throw new StorageException("Failed to store empty file.");
@@ -59,27 +67,49 @@ public class FileStorageService{
                         StandardCopyOption.REPLACE_EXISTING);
             }
         }
-        catch (IOException e) {
+        catch (IOException | StorageException e) {
             throw new StorageException("Failed to store file.", e);
         }
     }
 
-    public Stream<Path> loadAll() {
-        return Stream.empty();
+    public Stream<Path> loadAll() throws StorageException {
+        try {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(this.rootLocation::relativize);
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+
     }
 
 
     public Path load(String filename) {
-        return null;
+        return rootLocation.resolve(filename);
     }
 
 
-    public Resource loadAsResource(String filename) {
-        return null;
+    public Resource loadAsResource(String filename) throws StorageException {
+        try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+            else {
+                throw new StorageException(
+                        "Could not read file: " + filename);
+
+            }
+        }
+        catch (MalformedURLException | StorageException e) {
+            throw new StorageException("Could not read file: " + filename, e);
+        }
     }
 
 
     public void deleteAll() {
-
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
     }
 }
